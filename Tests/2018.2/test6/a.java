565cab7b9f35b65794464ad5f7dc87547a817017
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2018-10-17 21:15:43 -0500

    Create a.java

---
+++ Tests/2018.2/test6/a.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+//...
+
+public class a {
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.printf(
+            "%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%n%s", 
+            "Menu", "0.- salir", "1.- ejercicio 14.18",
+            "2.- ejercicio 14.20", "3.- ejercicio 14.21",
+            "4.- ejercicio 14.22", "5.- ejercicio 15.4",
+            "6.- ejercicio 15.5", "7.- ejercicio 15.6",
+            "8.- ejercicio 15.7", 
+            "Seleccione una de las opciones: ");
+            opcion = entrada.nextInt();
+            //...
+        } while(opcion != 0);
+    } 
+   
+    public static void p1418(){ //...
+    }
+    
+    public static void p1420(){ //...
+    }
+   
+    public static void p1421(){ //...
+    }
+   
+    public static void p1422(){ //...
+    }
+   
+    public static void p154(){ //...
+    }
+   
+    public static void p155(){ //...
+    }
+   
+    public static void p156(){ //...
+    }
+   
+    public static void p157(){ //...
+    }
+	 	 
+}
+
+//...
