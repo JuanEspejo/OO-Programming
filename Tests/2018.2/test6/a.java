28aa31902f1eae564618b3f4b432e225e2dc0019
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2018-10-17 21:31:50 -0500

    Update a.java

--- Tests/2018.2/test6/a.java
+++ Tests/2018.2/test6/a.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 //...
 
-public class a {
+public class a 
+{
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner entrada = new Scanner(System.in);
         int opcion;
-        do {
+        do 
+		{
             System.out.printf(
             "%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%s%n%n%s", 
             "Menu", "0.- salir", "1.- ejercicio 14.18",
@@ -20,28 +23,44 @@ public class a {
         } while(opcion != 0);
     } 
    
-    public static void p1418(){ //...
+    public static void p1418()
+    { 
+		//...
     }
     
-    public static void p1420(){ //...
+    public static void p1420()
+	{ 
+		//...
+	}
+   
+    public static void p1421()
+	{ 
+		//...
     }
    
-    public static void p1421(){ //...
+    public static void p1422()
+	{ 
+		//...
     }
    
-    public static void p1422(){ //...
+    public static void p154()
+	{ 
+		//...
     }
    
-    public static void p154(){ //...
+    public static void p155()
+	{ 
+		//...
     }
    
-    public static void p155(){ //...
+    public static void p156()
+	{ 
+	    //...
     }
    
-    public static void p156(){ //...
-    }
-   
-    public static void p157(){ //...
+    public static void p157()
+	{ 
+		//...
     }
 	 	 
 }
