0e87f42dfa2afaa8f590eb50b5f072eb93dc85d5
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-04-03 12:58:10 -0500

    Create BreakContinue.java

---
+++ Topics/Control Structures/BreakContinue.java
@@ -0,0 +1,25 @@
+public class BreakContinue {
+
+   public static void main(String[] args) {
+   /*
+      int count; // control variable also used after loop terminates
+      for (count = 1; count <= 10; count++) {  
+         if (count == 5)  
+            break; // terminates loop if count is 5
+         System.out.printf("%d ", count);
+      }
+      System.out.printf("%nBroke out of loop at count = %d%n", count);
+      
+      
+      for (int count = 1; count <= 10; count++) {  
+         if (count == 5) 
+            continue; // skip remaining code in loop body if count is 5
+         System.out.printf("%d ", count);
+      } 
+      System.out.printf("%nUsed continue to skip printing 5%n");
+   */   
+      int i = 0;
+      if(i != 0 & 10/i == 2);
+      
+   } 
+}
