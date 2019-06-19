50eb861fc73362615599f905994f9cea403996a3
Juan Carlos Espejo Delzo <jespejod@uni.edu.pe>
2019-06-19 12:20:07 -0500

    Create AutoBoxing.java

---
+++ Topics/Generic Collections/AutoBoxing.java
@@ -0,0 +1,7 @@
+public class Exam {       
+    public static void main(String[] args) {
+		Integer[] integerArray = new Integer[5];
+		integerArray[0] = 20;
+		int value = integerArray[0];
+    } 
+}
